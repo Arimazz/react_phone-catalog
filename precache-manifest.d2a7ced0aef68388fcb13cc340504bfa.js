98b58480a9f29a21d5e0482ae7ae339d96368402
Arimazz <bodyaprymyak@icloud.com>
2020-03-25 06:53:05 +0200

    Updates

---
+++ precache-manifest.d2a7ced0aef68388fcb13cc340504bfa.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ec3b3d89cae4b99a730fe3af127985d",
+    "url": "/react_phone-catalog/index.html"
+  },
+  {
+    "revision": "f6fc62404ee8fd25df5a",
+    "url": "/react_phone-catalog/static/css/main.a56db7dc.chunk.css"
+  },
+  {
+    "revision": "66ec30e6038c095ba5c1",
+    "url": "/react_phone-catalog/static/js/2.ed22739e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_phone-catalog/static/js/2.ed22739e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6fc62404ee8fd25df5a",
+    "url": "/react_phone-catalog/static/js/main.d936864f.chunk.js"
+  },
+  {
+    "revision": "e6af238f8195dfc6cde4",
+    "url": "/react_phone-catalog/static/js/runtime-main.0544dd7c.js"
+  },
+  {
+    "revision": "35ae702d95bc967034a2a9c15568e726",
+    "url": "/react_phone-catalog/static/media/Mont-ExtraLight.35ae702d.otf"
+  },
+  {
+    "revision": "7d90a574d6ef3b7949d79a56b4d201db",
+    "url": "/react_phone-catalog/static/media/Mont-Heavy.7d90a574.otf"
+  },
+  {
+    "revision": "d9a7f03afb31035369dcc27f61d24a90",
+    "url": "/react_phone-catalog/static/media/header__logo.d9a7f03a.svg"
+  }
+]);
